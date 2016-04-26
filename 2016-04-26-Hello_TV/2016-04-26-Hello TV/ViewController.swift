cfa64b1f935a92707fcd8ed7b88e27aff0c67bb0
thiagotmb <tmb0710@gmail.com>
2016-04-26 15:39:33 -0300

    Creating apple tv project

---
+++ 2016-04-26-Hello_TV/2016-04-26-Hello TV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2016-04-26-Hello TV
+//
+//  Created by Thiago-Bernardes on 4/26/16.
+//  Copyright © 2016 TB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
